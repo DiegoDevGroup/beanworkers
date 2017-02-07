917cfcf32a6b070b94b96dcdc9a82aa3eab479e6
John Congdon <john@johncongdon.com>
2017-02-06 23:27:17 -0800

    Initial code release.

---
+++ src/Supervisord/Beanstalkd/Workers.php
@@ -0,0 +1,78 @@
+<?php
+namespace Supervisord\Beanstalkd;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+use Pheanstalk\Pheanstalk;
+
+class Workers
+{
+    /** @var Pheanstalk */
+    protected $beanstalk;
+    protected $rules = [];
+    protected $num_proc_adjustment = false;
+    protected $supervisor_ctl = '/bin/supervisorctl';
+
+    /**
+     * Workers constructor.
+     * @param Pheanstalk $beanstalk
+     * @param string $supervisor_ctl Leave blank for testing
+     */
+    public function __construct(Pheanstalk $beanstalk, $supervisor_ctl = '/bin/supervisorctl')
+    {
+        $this->beanstalk      = $beanstalk;
+        $this->supervisor_ctl = $supervisor_ctl;
+    }
+
+    public function addRules($tube, $ini_file, $rules)
+    {
+        if ( ! isset($this->rules[$tube])) {
+            $this->rules[$tube] = [
+                'ini_file' => '',
+                'rules' => [],
+            ];
+        };
+
+        $this->rules[$tube]['ini_file'] = $ini_file;
+        $this->rules[$tube]['rules'][] = $rules;
+    }
+
+    public function run()
+    {
+        $tubes = $this->beanstalk->listTubes();
+        foreach ($tubes as $tube)
+        {
+            $stats = $this->beanstalk->statsTube($tube);
+
+            $this->adjustWorkers($tube, $stats);
+            if ($this->num_proc_adjustment && $this->supervisor_ctl) {
+                `{$this->supervisor_ctl} update $tube`;
+            }
+        }
+    }
+
+    protected function adjustWorkers($tube, $stats)
+    {
+        $this->num_proc_adjustment = false;
+        $current_jobs = $stats['current-jobs-ready'];
+        if ( ! isset($this->rules[$tube])) { return; }
+
+        $required_workers = 1;
+        foreach ($this->rules[$tube]['rules'] as $rule)
+        {
+            if ($current_jobs >= $rule['minimum_jobs']) {
+                $required_workers = $rule['required_workers'];
+            }
+        }
+
+        if (file_exists($this->rules[$tube]['ini_file'])) {
+            $file = file_get_contents($this->rules[$tube]['ini_file']);
+            preg_match('#numprocs=(\d)*#', $file, $matches);
+            if ($required_workers != $matches[1]) {
+                $file = preg_replace('#numprocs=\d*#', 'numprocs=' . $required_workers, $file);
+                file_put_contents($this->rules[$tube]['ini_file'], $file);
+                $this->num_proc_adjustment = true;
+            }
+        }
+    }
+}
---
+++ tests/Supervisord/Beanstalkd/Workers.php
@@ -0,0 +1,58 @@
+<?php
+namespace Supervisord\Beanstalkd;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+class WorkersTest extends \PHPUnit\Framework\TestCase
+{
+    protected $ini_file;
+    protected $beanstalk;
+    protected $worker;
+
+    public function setUp()
+    {
+        $this->ini_file = tempnam(sys_get_temp_dir(), 'supervisord');
+        file_put_contents($this->ini_file, $this->getExampleIniFile());
+        $this->beanstalk = $this->getMockBuilder('\Pheanstalk\Pheanstalk')
+            ->setConstructorArgs([''])
+            ->getMock();
+
+        $this->worker = new Workers($this->beanstalk, '');
+        $this->worker->addRules('default', $this->ini_file, ['minimum_jobs' => 0, 'required_workers' => 1]);
+        $this->worker->addRules('default', $this->ini_file, ['minimum_jobs' => 5, 'required_workers' => 3]);
+    }
+
+    public function testWorkersDecrease()
+    {
+        $this->beanstalk->expects($this->once())->method('listTubes')->will($this->returnValue(['default']));
+        $this->beanstalk->expects($this->once())->method('statsTube')->will($this->returnValue(['current-jobs-ready' => 2]));
+        $this->worker->run();
+
+        $file = file_get_contents($this->ini_file);
+        $this->assertContains('numprocs=1', $file, 'We should have 1 processes based on 2 current jobs');
+    }
+
+    public function testWorkersIncrease()
+    {
+
+        $this->beanstalk->expects($this->once())->method('listTubes')->will($this->returnValue(['default']));
+        $this->beanstalk->expects($this->once())->method('statsTube')->will($this->returnValue(['current-jobs-ready' => 5]));
+        $this->worker->run();
+
+        $file = file_get_contents($this->ini_file);
+        $this->assertContains('numprocs=3', $file, 'We should have 3 processes based on 5 current jobs');
+    }
+
+    protected function getExampleIniFile()
+    {
+        return <<<EOF
+[program:example]
+command=php example.php
+directory=/path/to/your/workers
+numprocs=5
+process_name=%(program_name)s_%(process_num)s
+autostart=true
+autorestart=true
+EOF;
+    }
+}
